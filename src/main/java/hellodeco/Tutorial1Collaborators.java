c06785b4819b72764b2eeb27a072115eb5badf82
Logan-De-Groot <logandegroot.9@gmail.com>
2022-08-01 20:57:35 +1000

    Added Logan De Groot (Logan-De-Groot) s46443762 to Tutorial1Collaborators

--- src/main/java/hellodeco/Tutorial1Collaborators.java
+++ src/main/java/hellodeco/Tutorial1Collaborators.java
@@ -25,6 +25,7 @@ public class Tutorial1Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Aaron Coox", "a-coox", "Tutor"));
         collaborators.add(new DecoPerson("Anastasia Laczko", "StasiaL", "Tutor"));
         collaborators.add(new DecoPerson("Guowei Yang", "guoweiyang", "Lecturer"));
+        collaborators.add(new DecoPerson("Logan De Groot", "Logan-De-Groot", "Student"));
         collaborators.add(new DecoPerson("Richard Thomas", "applebyter", "Course Coordinator"));
         collaborators.add(new DecoPerson("Rick Astley", "NeverGonnaGive", "Student"));
         collaborators.add(new DecoPerson("Samantha Tran", "samantha-tran", "Tutor"));
