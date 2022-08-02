34be8319e1d314533241352502d7c72155d76ebe
Connor Ryan <Work1998@hotmail.co.uk>
2022-08-02 16:55:35 +1000

    Added Connor Ryan (Connor-Ryan-1998) to Tutorial3Collaborators

--- src/main/java/hellodeco/Tutorial1Collaborators.java
+++ src/main/java/hellodeco/Tutorial1Collaborators.java
@@ -33,5 +33,7 @@ public class Tutorial1Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Rick Astley", "NeverGonnaGive", "Student"));
         collaborators.add(new DecoPerson("Samantha Tran", "samantha-tran", "Tutor"));
         collaborators.add(new DecoPerson("Zhiyuan Xu", "DLAmeng", "Student"));
+        collaborators.add(new DecoPerson("Connor Ryan", "Connor-Ryan-1998", "Student"));
+
     }
-} 
+}
