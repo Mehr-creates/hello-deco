46181e9994ca69a0785023a6af6cd4a77dbddd41
zihao-xia <490082085@qq.com>
2022-08-02 16:13:37 +0800

    Added Zihao Xia to ExternalTutorial1Collaborators

--- src/main/java/hellodeco/ExternalTutorial1Collaborators.java
+++ src/main/java/hellodeco/ExternalTutorial1Collaborators.java
@@ -31,6 +31,7 @@ public class ExternalTutorial1Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Samantha Tran2", "samantha-tran2", "Tutor"));
         collaborators.add(new DecoPerson("Xinkai Tang", "Kai9613", "Student"));
         collaborators.add(new DecoPerson("Yuqiao Tong", "CodingDavid2", "Student"));
+        collaborators.add(new DecoPerson("Zihao Xia", "zihao-xia", "Student"));
 
     }
 }
