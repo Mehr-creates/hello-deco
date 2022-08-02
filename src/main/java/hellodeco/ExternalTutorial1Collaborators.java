2b844ea045f30cf5757a4be6ce44e9a16d98a788
谭耀军 <tanyaojun@tanyaojundeAir.modem>
2022-08-02 18:00:35 +1000

    Added Yaojun Tan (PendaZz) 45048795 to ExternalTutorial1Collaborators

--- src/main/java/hellodeco/ExternalTutorial1Collaborators.java
+++ src/main/java/hellodeco/ExternalTutorial1Collaborators.java
@@ -31,12 +31,9 @@ public class ExternalTutorial1Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Samantha Tran", "samantha-tran", "Tutor"));
         collaborators.add(new DecoPerson("Samantha Tran2", "samantha-tran2", "Tutor"));
         collaborators.add(new DecoPerson("Xinkai Tang", "Kai9613", "Student"));
-<<<<<<< HEAD
         collaborators.add(new DecoPerson("Yaojun Tan", "PendaZz", "Student"));
-=======
         collaborators.add(new DecoPerson("Yingxin Liu", "LIU-Evelyn", "Student"));
         collaborators.add(new DecoPerson("Yuqiao Tong", "CodingDavid2", "Student"));
->>>>>>> 75c52ba4f51dd58957843a3fd343d428e20817cf
     }
 }
 
