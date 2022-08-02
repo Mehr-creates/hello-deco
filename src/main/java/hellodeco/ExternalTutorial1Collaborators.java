4c7c9ff97f299a8d379c56c914396c465f3f4f83
wsh <1771474353@qq.com>
2022-08-02 17:26:51 +0800

    fixed broken build + added test user

--- src/main/java/hellodeco/ExternalTutorial1Collaborators.java
+++ src/main/java/hellodeco/ExternalTutorial1Collaborators.java
@@ -30,11 +30,11 @@ public class ExternalTutorial1Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Rick Astley", "NeverGonnaGive", "Student"));
         collaborators.add(new DecoPerson("Samantha Tran", "samantha-tran", "Tutor"));
         collaborators.add(new DecoPerson("Samantha Tran2", "samantha-tran2", "Tutor"));
+        collaborators.add(new DecoPerson("Shuhui Wang", "Aryawwww", "Student"));
         collaborators.add(new DecoPerson("Xinkai Tang", "Kai9613", "Student"));
         collaborators.add(new DecoPerson("Yaojun Tan", "PendaZz", "Student"));
         collaborators.add(new DecoPerson("Yingxin Liu", "LIU-Evelyn", "Student"));
         collaborators.add(new DecoPerson("Yuqiao Tong", "CodingDavid2", "Student"));
-        collaborators.add(new DecoPerson("Shuhui Wang", "Aryawwww", "Student"));
         collaborators.add(new DecoPerson("Zihao Xia", "zihao-xia", "Student"));
     }
 }
