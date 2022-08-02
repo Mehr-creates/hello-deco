5db380f38288011a9025c044daa8ee24f6b3851f
kevinzheng <823688290@qq.com>
2022-08-03 03:30:30 +1000

    Added Kevin Zheng(Kebinzzz)(s4646474)to ExternalTutorial1Collaborators

--- src/main/java/hellodeco/ExternalTutorial1Collaborators.java
+++ src/main/java/hellodeco/ExternalTutorial1Collaborators.java
@@ -27,7 +27,7 @@ public class ExternalTutorial1Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Bohan Zhao", "Bohankkk", "Student"));
         collaborators.add(new DecoPerson("Guowei Yang", "guoweiyang", "Lecturer"));
         collaborators.add(new DecoPerson("Junwen Zhu", "JZ1890", "Student"));
-        collaborators.add(new DecoPerson("Kevin Zheng", "KevinZheng", "Student"));
+        collaborators.add(new DecoPerson("Kevin Zheng", "KevinZheng", "Students"));
         collaborators.add(new DecoPerson("Richard Thomas", "applebyter", "Course Coordinator"));
         collaborators.add(new DecoPerson("Rick Astley", "NeverGonnaGive", "Student"));
         collaborators.add(new DecoPerson("Samantha Tran", "samantha-tran", "Tutor"));
