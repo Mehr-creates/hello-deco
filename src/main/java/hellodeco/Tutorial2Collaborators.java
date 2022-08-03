8f9e7b9657673b393a1bafefb07f3fd676968f23
unknown <bach.truong@uqconnect.edu.au>
2022-08-03 16:15:52 +1000

    added Bach Truong to Tutorial 2

--- src/main/java/hellodeco/Tutorial2Collaborators.java
+++ src/main/java/hellodeco/Tutorial2Collaborators.java
@@ -36,6 +36,7 @@ public class Tutorial2Collaborators extends AbstractCollaboratorsList {
 		collaborators.add(new DecoPerson("Richard Thomas", "applebyter", "Course Coordinator"));
 		collaborators.add(new DecoPerson("Rick Astley", "NeverGonnaGive", "Student"));
 		collaborators.add(new DecoPerson("Satya Bhasale", "Satya Bhasale", "Student"));
+		collaborators.add(new DecoPerson("Bach Truong", "bach-truong", "Student"));
 
 	}
 }
