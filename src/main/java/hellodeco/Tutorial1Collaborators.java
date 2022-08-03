854fbf685e703a3a4b6244d1463ca56800f73d87
Yingzheng Cai <yingzhengcai@d-i89-150-175.student.eduroam.uq.edu.au>
2022-08-03 14:56:08 +1000

    Added Yingzheng Cai (Rey-666) 46822497 to Tutorial1Collaborators

--- src/main/java/hellodeco/Tutorial1Collaborators.java
+++ src/main/java/hellodeco/Tutorial1Collaborators.java
@@ -42,8 +42,8 @@ public class Tutorial1Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Samantha Tran", "samantha-tran", "Tutor"));
         collaborators.add(new DecoPerson("Satya Bhasale", "Satya Bhasale", "Student"));
         collaborators.add(new DecoPerson("Shiyu Liu", "11shiyu", "Student"));
-        collaborators.add(new DecoPerson("Yingzheng Cai", "Rey-666", "Student"));
         collaborators.add(new DecoPerson("Tara-Jade Garnsworthy", "tgarnsworthy", "Student"));
+        collaborators.add(new DecoPerson("Yingzheng Cai", "Rey-666", "Student"));
         collaborators.add(new DecoPerson("Zhiyuan Xu", "DLAmeng", "Student"));
 
     }
