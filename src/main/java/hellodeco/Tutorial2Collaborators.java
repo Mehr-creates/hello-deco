d9e2b6ce1756202264eb0e97d5c8c0d8d56ce0c3
taylaward <tayla.r.ward@outlook.com>
2022-08-04 09:51:29 +1000

    Added Tayla Ward (taylaward) 46636331 to Tutorial2Collaborators

--- src/main/java/hellodeco/Tutorial2Collaborators.java
+++ src/main/java/hellodeco/Tutorial2Collaborators.java
@@ -37,6 +37,7 @@ public class Tutorial2Collaborators extends AbstractCollaboratorsList {
 		collaborators.add(new DecoPerson("Richard Thomas", "applebyter", "Course Coordinator"));
 		collaborators.add(new DecoPerson("Rick Astley", "NeverGonnaGive", "Student"));
 		collaborators.add(new DecoPerson("Satya Bhasale", "Satya Bhasale", "Student"));
+		collaborators.add(new DecoPerson("Tayla Ward", "taylaward", "Student"));
 
 
 	}
