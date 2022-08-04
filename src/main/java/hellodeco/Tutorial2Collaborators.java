c38578e89a3be8765637cc6c5d829200086d0554
lalala66888 <105657785+lalala66888@users.noreply.github.com>
2022-08-04 13:09:48 +1000

    add wanqi to tut2

--- src/main/java/hellodeco/Tutorial2Collaborators.java
+++ src/main/java/hellodeco/Tutorial2Collaborators.java
@@ -27,6 +27,7 @@ public class Tutorial2Collaborators extends AbstractCollaboratorsList {
 		collaborators.add(new DecoPerson("Ari Bowe", "AriBowe", "Tutor"));
 		collaborators.add(new DecoPerson("Bach Truong", "bach-truong", "Student"));
 		collaborators.add(new DecoPerson("Cheng-Yu Lin", "Lumin-cider", "Student"));
+		collaborators.add(new DecoPerson("Chol Nhial Chol", "cholnhial", "Student"));
 		collaborators.add(new DecoPerson("Connor Ryan", "Connor-Ryan-1998", "Student"));
 		collaborators.add(new DecoPerson("George Mihnea", "BozenX", "Tutor"));
 		collaborators.add(new DecoPerson("Guowei Yang", "guoweiyang", "Lecturer"));
@@ -39,8 +40,7 @@ public class Tutorial2Collaborators extends AbstractCollaboratorsList {
 		collaborators.add(new DecoPerson("Rick Astley", "NeverGonnaGive", "Student"));
 		collaborators.add(new DecoPerson("Satya Bhasale", "Satya Bhasale", "Student"));
 		collaborators.add(new DecoPerson("Tayla Ward", "taylaward", "Student"));
-		collaborators.add(new DecoPerson("Chol Nhial Chol", "cholnhial", "Student"));
-
+		collaborators.add(new DecoPerson("Wanqi Ma", "lalala66888", "Student"));
 
 	}
 }
