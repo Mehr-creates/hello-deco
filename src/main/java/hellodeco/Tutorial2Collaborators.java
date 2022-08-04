e00d7047915b4322f4f6bd49588ce084d5960a2f
Yashvardhan Batra <yashvardhanbatra410@gmail.com>
2022-08-04 16:30:59 +1000

    Yashvardhan Batra added to collaborators

--- src/main/java/hellodeco/Tutorial2Collaborators.java
+++ src/main/java/hellodeco/Tutorial2Collaborators.java
@@ -38,6 +38,8 @@ public class Tutorial2Collaborators extends AbstractCollaboratorsList {
 		collaborators.add(new DecoPerson("Richard Thomas", "applebyter", "Course Coordinator"));
 		collaborators.add(new DecoPerson("Rick Astley", "NeverGonnaGive", "Student"));
 		collaborators.add(new DecoPerson("Satya Bhasale", "Satya Bhasale", "Student"));
+		collaborators.add(new DecoPerson("Yashvardhan Batra", "yashvardhanbatra", "Student"));
+
 
 
 	}
