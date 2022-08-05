68b1efd6d1af7a0a1d7c96392e4724f879949651
nicococole <w.shan@uqconnect.edu.au>
2022-08-05 15:22:33 +1000

    add new student

--- src/main/java/hellodeco/Tutorial3Collaborators.java
+++ src/main/java/hellodeco/Tutorial3Collaborators.java
@@ -37,5 +37,6 @@ public class Tutorial3Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Sylvia Chen", "syl2211","Student"));
         collaborators.add(new DecoPerson("Tyler Tevita", "Tyler", "Student2"));
         collaborators.add(new DecoPerson("Viktor Milanov", "Viktor Milanov", "Student2"));
+        collaborators.add(new DecoPerson("Wenjing Shan", "wenjing", "Student"))
     }
 }
