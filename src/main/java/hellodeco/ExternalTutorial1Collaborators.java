cba99e5c7fb20f71947d81b9460b4021f422c6da
zll <1500709840@qq.com>
2022-08-05 10:57:00 +0800

    added luluzhong s4762108 to externalTUT

--- src/main/java/hellodeco/ExternalTutorial1Collaborators.java
+++ src/main/java/hellodeco/ExternalTutorial1Collaborators.java
@@ -34,6 +34,7 @@ public class ExternalTutorial1Collaborators extends AbstractCollaboratorsList {
         collaborators.add(new DecoPerson("Junyu Zhu", "45190331", "Student"));
         collaborators.add(new DecoPerson("Kevin Zheng", "KevinZheng", "Students"));
         collaborators.add(new DecoPerson("Kexin Du", "motaA0309", "Students"));
+        collaborators.add(new DecoPerson("Lulu Zhong", "lily441", "Student"));
         collaborators.add(new DecoPerson("Qiansen Jin", "QiansenJhin", "Student"));
         collaborators.add(new DecoPerson("Qicheng Chen", "Wayneecc", "Student"));
         collaborators.add(new DecoPerson("Qing Zhang", "Versprechen080", "Student"));
